0884ebb69b733264a0c5681a956ede7b441fadd1
Zane Cotten <zane@zanecotten.dev>
2026-06-29 12:09:03 -0500

    refactor: extract common methods from resultsView and created parent View class

--- final/src/js/views/recipeView.js
+++ final/src/js/views/recipeView.js
@@ -1,79 +1,39 @@
 import toFraction from 'fracty';
 import icons from 'url:../../img/icons.svg';
+import { View } from './View';
 
-class RecipeView {
-  #parentElement = document.querySelector('.recipe');
-  #data;
-  #errorMessage = 'Recipe not found :(';
-  #message = '';
+class RecipeView extends View {
+    _parentElement = document.querySelector('.recipe');
+    _data;
+    _message;
 
-  //////////////////////////////////////////////
-  // PUBLIC METHODS
-  //////////////////////////////////////////////
+    //////////////////////////////////////////////
+    // PUBLIC METHODS
+    //////////////////////////////////////////////
 
-  // Displays current recipe
-  render(data) {
-    this.#data = data;
+    // Gets hash from address bar (the currently selected recipe ID)
+    getHash() {
+        return window.location.hash.slice(1);
+    }
 
-    this.#clear();
-    const markup = this.#generateMarkup();
-    this.#insertMarkup(markup);
-  }
+    // Publisher for render methods
+    addHandlerRender(callback) {
+        ['hashchange', 'load'].forEach(event =>
+            window.addEventListener(event, callback),
+        );
+    }
 
-  // Displays spinner
-  renderSpinner() {
-    this.#clear();
-    const markup = this.#generateSpinnerMarkup();
-    this.#insertMarkup(markup);
-  }
+    //////////////////////////////////////////////
+    // PROTECTED METHODS
+    //////////////////////////////////////////////
 
-  // Gets hash from address bar (the currently selected recipe ID)
-  getHash() {
-    return window.location.hash.slice(1);
-  }
-
-  // Displays error message to user
-  renderError(err) {
-    this.#clear();
-    const markup = this.#generateErrorMarkup(err);
-    this.#insertMarkup(markup);
-  }
-
-  // Displays message to user
-  renderMessage(message) {
-    this.#clear();
-    const markup = this.#generateMessageMarkup(message);
-    this.#insertMarkup(markup);
-  }
-
-  // Publisher for render methods
-  addHandlerRender(callback) {
-    ['hashchange', 'load'].forEach(event =>
-      window.addEventListener(event, callback),
-    );
-  }
-
-  //////////////////////////////////////////////
-  // PRIVATE METHODS
-  //////////////////////////////////////////////
-
-  // Clears inner HTML of parent class
-  #clear() {
-    this.#parentElement.innerHTML = '';
-  }
-
-  // Inserts markup into parent element at insert position
-  #insertMarkup(markup, insertPosition = 'afterbegin') {
-    this.#parentElement.insertAdjacentHTML(insertPosition, markup);
-  }
-
-  // Returns Markup using the current data (recipe)
-  #generateMarkup() {
-    return `
-      <figure class="recipe__fig">
-      <img src="${this.#data.imageUrl}" alt="${this.#data.title}" class="recipe__img" />
+    // Returns Markup using the current data (recipe)
+    _generateMarkup() {
+        return `
+        <figure class="recipe__fig">
+      <img src="${this._data.imageUrl}" alt="${this._data.title}" class="recipe__img" />
       <h1 class="recipe__title">
-      <span>${this.#data.title}</span>
+      <span>${this._data.title}</span>
       </h1>
       </figure>
       
@@ -82,14 +42,14 @@ class RecipeView {
       <svg class="recipe__info-icon">
       <use href="${icons}.svg#icon-clock"></use>
       </svg>
-      <span class="recipe__info-data recipe__info-data--minutes">${this.#data.cookingTime}</span>
+      <span class="recipe__info-data recipe__info-data--minutes">${this._data.cookingTime}</span>
       <span class="recipe__info-text">minutes</span>
       </div>
       <div class="recipe__info">
       <svg class="recipe__info-icon">
       <use href="${icons}.svg#icon-users"></use>
       </svg>
-      <span class="recipe__info-data recipe__info-data--people">${this.#data.servings}</span>
+      <span class="recipe__info-data recipe__info-data--people">${this._data.servings}</span>
       <span class="recipe__info-text">servings</span>
       
       <div class="recipe__info-buttons">
@@ -121,7 +81,7 @@ class RecipeView {
       <div class="recipe__ingredients">
       <h2 class="heading--2">Recipe ingredients</h2>
       <ul class="recipe__ingredient-list">
-      ${this.#data.ingredients.map(ingredient => this.#generateIngredientMarkup(ingredient)).join('')}
+      ${this._data.ingredients.map(ingredient => this.#generateIngredientMarkup(ingredient)).join('')}
       </ul>
       </div>
       
@@ -129,12 +89,12 @@ class RecipeView {
       <h2 class="heading--2">How to cook it</h2>
       <p class="recipe__directions-text">
       This recipe was carefully designed and tested by
-      <span class="recipe__publisher">${this.#data.publisher}</span>.
+      <span class="recipe__publisher">${this._data.publisher}</span>.
       Please check out directions at their website.
       </p>
       <a 
       class="btn--small recipe__btn"
-      href="${this.#data.sourceUrl}"
+      href="${this._data.sourceUrl}"
       target="_blank"
       >
       <span>Directions</span>
@@ -144,11 +104,15 @@ class RecipeView {
       </a>
       </div>
       `;
-  }
+    }
 
-  // Returns Markup for the ingredient received
-  #generateIngredientMarkup(ingredient) {
-    return `
+    //////////////////////////////////////////////
+    // PRIVATE METHODS
+    //////////////////////////////////////////////
+
+    // Returns Markup for the ingredient received
+    #generateIngredientMarkup(ingredient) {
+        return `
         <li class="recipe__ingredient">
           <svg class="recipe__icon">
             <use href="${icons}.svg#icon-check"></use>
@@ -160,60 +124,15 @@ class RecipeView {
           </div>
         </li>
       `;
-  }
+    }
 
-  // Returns Markup for ingredient quantity received
-  // If quantity is undefined, returns empty string
-  #generateIngredientQuantityMarkup(quantity) {
-    return quantity
-      ? `<div class="recipe__quantity">${toFraction(quantity)}</div>`
-      : '';
-  }
-
-  #generateSpinnerMarkup() {
-    return `
-    <div class="spinner">
-      <svg>
-        <use href="${icons}#icon-loader"></use>
-      </svg>
-    </div>
-  `;
-  }
-
-  #escapeHtml(value) {
-    return String(value)
-      .replace(/&/g, '&amp;')
-      .replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;')
-      .replace(/"/g, '&quot;')
-      .replace(/'/g, '&#39;');
-  }
-
-  #generateErrorMarkup(error, errorMessage) {
-    const safeErrorMessage = this.#escapeHtml(error?.message ?? '');
-
-    return `
-      <div style="text-align: center; margin: 5rem;">
-        <h1>${this.#errorMessage}</h1>
-        <p>${safeErrorMessage}</p>
-      </div>
-    `;
-  }
-
-  #generateMessageMarkup(message = this.#message) {
-    return `
-    <div class="message">
-        <div>
-            <svg>
-                <use href="${icons}#icon-smile"></use>
-            </svg>
-        </div>
-        <p>
-            ${message}
-        </p>
-    </div>
-    `;
-  }
+    // Returns Markup for ingredient quantity received
+    // If quantity is undefined, returns empty string
+    #generateIngredientQuantityMarkup(quantity) {
+        return quantity
+            ? `<div class="recipe__quantity">${toFraction(quantity)}</div>`
+            : '';
+    }
 }
 
 export default new RecipeView();

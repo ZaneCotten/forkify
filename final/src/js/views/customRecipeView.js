7e725817254218e20f73c4bddad40d8bcff2b679
Zane Cotten <zane@zanecotten.dev>
2026-07-01 17:15:33 -0500

    wip: remove test values from custom recipe form

--- final/src/js/views/customRecipeView.js
+++ final/src/js/views/customRecipeView.js
@@ -52,38 +52,32 @@ class CustomRecipeView extends View {
         this._submitCallback(data);
     };
 
-    _clearForm() {
-        // this._allInputs.forEach(input => (input.value = ''));
-    }
-
     _generateMarkup() {
         return `
                 <div class="upload__column">
                     <h3 class="upload__heading">Recipe data</h3>
                     <label>Title</label>
-                    <input value="TEST" required name="title" type="text" />
+                    <input required name="title" type="text" />
                     <label>URL</label>
-                    <input value="TEST" required name="sourceUrl" type="text" />
+                    <input required name="sourceUrl" type="text" />
                     <label>Image URL</label>
-                    <input value="TEST" required name="image" type="text" />
+                    <input required name="image" type="text" />
                     <label>Publisher</label>
-                    <input value="TEST" required name="publisher" type="text" />
+                    <input required name="publisher" type="text" />
                     <label>Prep time</label>
                     <input
-                        value="23"
                         required
                         name="cookingTime"
                         type="number"
                     />
                     <label>Servings</label>
-                    <input value="23" required name="servings" type="number" />
+                    <input required name="servings" type="number" />
                 </div>
 
                 <div class="upload__column">
                     <h3 class="upload__heading">Ingredients</h3>
                     <label>Ingredient 1</label>
                     <input
-                        value="0.5,kg,Rice"
                         type="text"
                         required
                         name="ingredient-1"
@@ -91,14 +85,12 @@ class CustomRecipeView extends View {
                     />
                     <label>Ingredient 2</label>
                     <input
-                        value="1,,Avocado"
                         type="text"
                         name="ingredient-2"
                         placeholder="Format: 'Quantity,Unit,Description'"
                     />
                     <label>Ingredient 3</label>
                     <input
-                        value=",,salt"
                         type="text"
                         name="ingredient-3"
                         placeholder="Format: 'Quantity,Unit,Description'"
